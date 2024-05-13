1eb047c7be721adae051aa77f86e11ce56b89c36
Lendulka <cernohorska.lc@gmail.com>
2024-05-13 23:59:35 +0200

    3 fáze

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -8,30 +8,36 @@ interface IWordboxProp {
 
 const Wordbox: React.FC<IWordboxProp> = ({ word, onFinish }) => {
   const [lettersLeft, setLettersLeft] = useState<string>(word);
+  const [mistake, setMistake] = useState<boolean>(false)
 
   console.log(lettersLeft)
 
   const handleLetters = (e: KeyboardEvent) => {
     if (lettersLeft.length > 0) {
       if (e.key === lettersLeft[0]) {
+        setMistake(false)
         if (lettersLeft.length === 1) {
           onFinish()
         } else {
           setLettersLeft(lettersLeft.slice(1))
         }
+      } else {
+        setMistake(true)
       }
     }
   }
 
   useEffect(() => {
+    console.log("spuštění efektu")
+    console.log(mistake, lettersLeft)
     document.addEventListener("keyup", handleLetters)
     return () => {
       document.removeEventListener("keyup", handleLetters)
     }
-  }, [lettersLeft])
+  }, [lettersLeft, mistake])
 
   return (
-    <div className="wordbox">{lettersLeft}</div>
+    <div className={`wordbox ${mistake && "wordbox--mistake"}`}>{lettersLeft}</div>
   );
 };
 
